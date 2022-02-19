4a0d40806f0fb36d73b60763f05e012825bb0931
Peter Chacko <workpraytoday@gmail.com>
2022-02-18 18:56:39 -0600

    deleted template project

--- template-project/build_tools/__init__.py
+++
@@ -1,8 +0,0 @@
-# aruw Python build tools
-__all__ = [
-    "extract_robot_type",
-    "parse_args"
-]
-
-from . import extract_robot_type
-from . import parse_args
--- template-project/robot-type/robot_type.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __ROBOT_TYPE_HPP__
-#define __ROBOT_TYPE_HPP__
-#define TARGET_STANDARD
-#endif
