cc2a146993d0c316474299e01dfa36e92c42acb9
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2022-10-23 16:19:31 -0500

    Update chassis_subsystem.cpp
    Changed to four motors

--- ut-robomaster/src/subsystems/chassis/chassis_subsystem.cpp
+++ ut-robomaster/src/subsystems/chassis/chassis_subsystem.cpp
@@ -17,47 +17,84 @@
  * along with taproot-examples.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "chassis_subsystem.hpp"
+/**
+ * This is part of aruw's library.
+ *
+ * This is example code for running friction wheels. As you can
+ * see, there is a generic update pid loop that is independent of
+ * what command is given to the subsystem. Additionally, the
+ * subsystem contains variables specific to the subsystem
+ * (pid controllers, motors, etc). If a control loop is specific
+ * to a command, it should NOT be in a subsystem. For example,
+ * control code to pulse the friction wheels should be located
+ * outside of this class because pulsing is a specific command.
+ */
 
-#include "drivers.hpp"
+#ifndef EXAMPLE_SUBSYSTEM_HPP_
+#define EXAMPLE_SUBSYSTEM_HPP_
 
-ChassisSubsystem::ChassisSubsystem(
-    Drivers* drivers,
-    tap::motor::MotorId leftMotorId,
-    tap::motor::MotorId rightMotorId)
-    : tap::control::Subsystem(drivers),
-      velocityPidLeftWheel(PID_P, PID_I, PID_D, PID_MAX_ERROR_SUM, PID_MAX_OUTPUT),
-      velocityPidRightWheel(PID_P, PID_I, PID_D, PID_MAX_ERROR_SUM, PID_MAX_OUTPUT),
-      desiredRpm(0),
-      leftWheel(drivers, leftMotorId, CAN_BUS_MOTORS, true, "left example motor"),
-      rightWheel(drivers, rightMotorId, CAN_BUS_MOTORS, false, "right example motor")
+#include "tap/control/command_scheduler.hpp"
+#include "tap/control/subsystem.hpp"
+
+#include "tap/motor/dji_motor.hpp"
+
+#include "modm/math/filter/pid.hpp"
+
+class Drivers;
+
+class ChassisSubsystem : public tap::control::Subsystem
 {
-}
+public:
+    ChassisSubsystem(
+        tap::Drivers* drivers,
+        tap::motor::MotorId leftFrontMotorId = tap::motor::MOTOR1,
+        tap::motor::MotorId rightFrontMotorId = tap::motor::MOTOR2,
+        tap::motor::MotorId leftBackMotorId = tap::motor::MOTOR3,
+        tap::motor::MotorId rightBackMotorId = tap::motor::MOTOR4);
 
-void ChassisSubsystem::initialize()
-{
-    leftWheel.initialize();
-    rightWheel.initialize();
-}
+    void initialize() override;
 
-void ChassisSubsystem::setDesiredRpm(float desRpm) { desiredRpm = desRpm; }
+    void setDesiredRpm(float desRpm);
 
-void ChassisSubsystem::refresh()
-{
-    updateMotorRpmPid(&velocityPidLeftWheel, &leftWheel, desiredRpm);
-    updateMotorRpmPid(&velocityPidRightWheel, &rightWheel, desiredRpm);
-}
+    void refresh() override;
 
-void ChassisSubsystem::updateMotorRpmPid(
-    modm::Pid<float>* pid,
-    tap::motor::DjiMotor* motor,
-    float desiredRpm)
-{
-    pid->update(desiredRpm - motor->getShaftRPM());
-    motor->setDesiredOutput(static_cast<int32_t>(pid->getValue()));
-}
+    void runHardwareTests() override;
 
-void ChassisSubsystem::runHardwareTests()
-{
-    // TODO
-}
+    const char* getName() override { return "Chassis subsystem"; }
+
+private:
+    static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
+
+    static constexpr float PID_P = 5.0f;
+    static constexpr float PID_I = 0.0f;
+    static constexpr float PID_D = 1.0f;
+    static constexpr float PID_MAX_ERROR_SUM = 0.0f;
+    static constexpr float PID_MAX_OUTPUT = 16000;
+
+    modm::Pid<float> velocityPidLeftFrontWheel;
+    modm::Pid<float> velocityPidLeftBackWheel;
+    modm::Pid<float> velocityPidRightFrontWheel;
+    modm::Pid<float> velocityPidRightBackWheel;
+
+    float desiredRpm[4];
+
+    void updateMotorRpmPid(
+        modm::Pid<float>* pid,
+        tap::motor::DjiMotor* const motor,
+        float desiredRpm);
+
+#if defined(PLATFORM_HOSTED) && defined(ENV_UNIT_TESTS)
+public:
+    tap::mock::DjiMotorMock leftWheel;
+    tap::mock::DjiMotorMock rightWheel;
+
+private:
+#else
+    tap::motor::DjiMotor leftFrontWheel;
+    tap::motor::DjiMotor leftBackWheel;
+    tap::motor::DjiMotor rightFrontWheel;
+    tap::motor::DjiMotor rightBackWheel;
+#endif
+};
+
+#endif  // EXAMPLE_SUBSYSTEM_HPP_
