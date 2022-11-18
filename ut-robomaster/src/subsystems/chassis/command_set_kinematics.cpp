3ea677608fed33591c89c02f5c85c80577c52fb0
Caleb Chalmers <caleb.chalmers1@gmail.com>
2022-11-17 18:26:36 -0600

    Initial stuff (doesn't work)

--- ut-robomaster/src/subsystems/chassis/command_set_kinematics.cpp
+++ ut-robomaster/src/subsystems/chassis/command_set_kinematics.cpp
@@ -8,84 +8,94 @@ void SetKinematicsCommand::initialize() {}
 
 void SetKinematicsCommand::execute()
 {
-    Remote* remote = &drivers->remote;
-    Remote::SwitchState rightSwitchState = remote->getSwitch(Remote::Switch::RIGHT_SWITCH);
-
-    if (rightSwitchState == Remote::SwitchState::UP)  // controller input mode
+    if (drivers->bmi088.getImuState() == ImuInterface::ImuState::IMU_CALIBRATING && false)
     {
-        Vector2f moveInput = Vector2f(
-            remote->getChannel(Remote::Channel::RIGHT_HORIZONTAL),
-            -remote->getChannel(Remote::Channel::RIGHT_VERTICAL));
-        float spinInput = remote->getChannel(Remote::Channel::LEFT_HORIZONTAL);
-
-        if (moveInput.getLengthSquared() > ANALOG_DEAD_ZONE * ANALOG_DEAD_ZONE)
-        {
-            velocity = moveInput;
-        }
-        else
-        {
-            velocity = Vector2f(0.0f);
-        }
-
-        wZ = (abs(spinInput) > ANALOG_DEAD_ZONE) ? spinInput : 0.0f;
+        subsystem->setVelocities(Vector2f(0.0f), 0.0f);
     }
-    else  // keyboard input mode
+    else
     {
-        if (remote->keyPressed(Remote::Key::R) != isRKeyPressed)
+        bool useControllerInput =
+            drivers->remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::UP;
+
+        if (useControllerInput)
+            doControllerInput();
+        else
+            doKeyboardInput();
+
+        // compute input clamping per the spin factor
+        float e = max(0.0f, inputMove.getLength() + abs(inputSpin) - 1.0f);
+        Vector2f clampedMove = inputMove - inputMove.normalized() * e * SPIN_FACTOR;
+        float clampedSpin = inputSpin - e * (1.0f - SPIN_FACTOR);
+
+                subsystem->setVelocities(cv, cwZ);
+    }
+}
+
+void SetKinematicsCommand::end(bool) { subsystem->setVelocities(Vector2f(0.0f), 0.0f); }
+bool SetKinematicsCommand::isFinished() const { return false; }
+
+void SetKinematicsCommand::doControllerInput()
+{
+    Remote* remote = &drivers->remote;
+    inputMove = Vector2f(
+        remote->getChannel(Remote::Channel::RIGHT_HORIZONTAL),
+        -remote->getChannel(Remote::Channel::RIGHT_VERTICAL));
+    inputSpin = -static_cast<float>(remote->getWheel()) / 660.0f;  // 660 is the max
+
+    float inputMoveLen = inputMove.getLength();
+    if (inputMoveLen < ANALOG_DEAD_ZONE)
+    {
+        inputMove = Vector2f(0.0f);
+    }
+    else
+    {
+        inputMove /= max(1.0f, inputMove.getLength());  // clamp length
+    }
+
+    if (abs(inputSpin) < ANALOG_DEAD_ZONE)
+    {
+        inputSpin = 0.0f;
+    }
+}
+
+void SetKinematicsCommand::doKeyboardInput()
+{
+    Remote* remote = &drivers->remote;
+    bool isRKeyPressed = remote->keyPressed(Remote::Key::R);
+    if (isRKeyPressed != wasRKeyPressed)
+    {
+        wasRKeyPressed = isRKeyPressed;
+        if (isRKeyPressed)
         {
-            isRKeyPressed = remote->keyPressed(Remote::Key::R);
-            if (isRKeyPressed) subsystem->isBeyblading = !subsystem->isBeyblading;
-        }
-
-        Vector2f input = Vector2f(0.0f);
-
-        if (remote->keyPressed(Remote::Key::A))
-        {
-            input.x -= 1.0f;
-        }
-
-        if (remote->keyPressed(Remote::Key::D))
-        {
-            input.x += 1.0f;
-        }
-
-        if (remote->keyPressed(Remote::Key::W))
-        {
-            input.y += 1.0f;
-        }
-
-        if (remote->keyPressed(Remote::Key::S))
-        {
-            input.y -= 1.0f;
-        }
-
-        velocity += input * KEYBOARD_ACCEL * DELTA_TIME;
-
-        // clamp velocities
-        velocity /= modm::max(1.0f, velocity.getLength());
-
-        // decelerate when input stops
-        if (input.getLengthSquared() < 0.1f)
-        {
-            float velLen = velocity.getLength();
-            if (velLen > 0.0f)
-            {
-                velocity *= max(1.0f - KEYBOARD_DECEL * DELTA_TIME / velLen, 0.0f);
-            }
+            subsystem->isBeyblading = !subsystem->isBeyblading;
         }
     }
 
     if (subsystem->isBeyblading)
     {
-        velocity *= subsystem->TRANSLATION_TO_ROTATION_RATIO;
-        wZ = 1 - subsystem->TRANSLATION_TO_ROTATION_RATIO;
+        inputSpin = 1.0f;
     }
 
-    subsystem->setVelocities(velocity.x, velocity.y, wZ);
+    Vector2f rawMoveInput = Vector2f(
+        remote->keyPressed(Remote::Key::D) - remote->keyPressed(Remote::Key::A),
+        remote->keyPressed(Remote::Key::W) - remote->keyPressed(Remote::Key::S));
+    float rawInputLen = rawMoveInput.getLength();
+
+    if (rawInputLen > 0.0f)
+    {
+        Vector2f moveDir = rawMoveInput / rawInputLen;       // normalize input
+        inputMove += moveDir * KEYBOARD_ACCEL * DELTA_TIME;  // incorporate input
+        inputMove /= max(1.0f, inputMove.getLength());       // clamp length
+    }
+    else
+    {
+        // decelerate when input stops
+        float len = inputMove.getLength();
+        if (len > 0.0f)
+        {
+            inputMove *= max(1.0f - KEYBOARD_DECEL * DELTA_TIME / len, 0.0f);
+        }
+    }
 }
-
-void SetKinematicsCommand::end(bool) { subsystem->setVelocities(0.0f, 0.0f, 0.0f); }
-
-bool SetKinematicsCommand::isFinished() const { return false; }
 }  // namespace chassis
 }  // namespace subsystems
