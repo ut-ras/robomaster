d8e9cde782fe054e5612505dfb364bc1913200b3
Caleb Chalmers <caleb.chalmers1@gmail.com>
2023-11-21 23:34:30 +0000

    Remove outdated/incorrect copyright notices

--- ut-robomaster/src/drivers.hpp
+++ ut-robomaster/src/drivers.hpp
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2021-2022 UT Robomaster
- *
- * This file is part of ut-robomaster.
- *
- * ut-robomaster is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ut-robomaster is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ut-robomaster.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 #ifndef DRIVERS_HPP_
 #define DRIVERS_HPP_
 
