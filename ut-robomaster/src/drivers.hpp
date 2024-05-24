3f01a5bb2454f65799ac025490d46e4d1883d7a5
Caleb Chalmers <caleb.chalmers1@gmail.com>
2024-05-24 03:14:57 +0000

    Remove unused robotcomms from drivers

--- ut-robomaster/src/drivers.hpp
+++ ut-robomaster/src/drivers.hpp
@@ -3,16 +3,15 @@
 #include "tap/drivers.hpp"
 
 #include "communication/cv_board.hpp"
-#include "utils/robot_comms.hpp"
 
 namespace src
 {
 class Drivers : public tap::Drivers
 {
 public:
-    Drivers() : tap::Drivers(), cvBoard(this), terminal(this) {}
+    Drivers() : tap::Drivers(), cvBoard(this) {}
     communication::CVBoard cvBoard;
-    comms::RobotComms terminal;
+
     bool isKillSwitched() { return !remote.isConnected(); }
 };  // class Drivers
 
