a8d7889212c1de7f7c714d23a87b7c361d4a4c90
Caleb Chalmers <caleb.chalmers1@gmail.com>
2024-11-26 05:30:39 +0000

    Use MilliTimeout for sentry spin timer

--- ut-robomaster/src/subsystems/chassis/command_sentry_position.cpp
+++ ut-robomaster/src/subsystems/chassis/command_sentry_position.cpp
@@ -3,70 +3,24 @@
 namespace commands
 {
 
-void CommandSentryPosition::initialize() { startup_time = getTimeMilliseconds(); }
+void CommandSentryPosition::initialize() { moveTimer.stop(); }
 
 void CommandSentryPosition::execute()
 {
-    Vector2f move = Vector2f(0.0f);
-    float spin = 0.0f;
-
-    if (drivers->isGameActive() && !gameStarted)
+    // wait until game starts then wait some more to avoid excess drifting
+    if (drivers->isGameActive() && moveTimer.isStopped())
     {
-        gameStarted = true;
-        startup_time = getTimeMilliseconds();
+        moveTimer.restart(10'000);  // 10s
     }
 
-    // if (prevTime == 0.0f)
-    // {
-    //     prev_time = getTimeMilliseconds();
-    // }
-
-    // if (getTimeMilliseconds() - prev_time > 5000.0f)
-    // {
-    //     direction *= -1;
-    //     prev_time = getTimeMilliseconds();
-    // }
-
-    // if (!isStarted)
-    // {
-    //     isStarted = true;
-    //     startTime = getTimeMilliseconds() / 1000.0f;
-    // }
-
-    // float time = getTimeMilliseconds() / 1000.0f - startTime;
-
-    // if (time < 2.0f)
-    // {
-    //     float t = time / 2.0f;
-    //     float fac = 4.0f * (1.0f - t) * t;
-    //     move = Vector2f(0.0f, fac * 0.1f);
-    // }
-    // else if (time < 3.0f)
-    // {
-    //     spin = 0.2f;
-    // }
-
-    spin = 1.0f;
-
-    if (gameStarted && getTimeMilliseconds() - startup_time < 35000.0f)
-    {
-        spin = 0.0f;
-    }
-
-    // don't move for 10 sec
-    if (gameStarted && getTimeMilliseconds() - startup_time < 10000.f)
-    {
-        move = Vector2f(0.0f);
-    }
-
-    if (drivers->isGameActive())
-    {
-        chassis->input(move, spin);
-    }
-    else
+    if (!moveTimer.isExpired())
     {
         chassis->input(Vector2f(0.0f), 0.0f);
+        return;
     }
+
+    // speen
+    chassis->input(Vector2f(0.0f), 1.0f);
 }
 
 void CommandSentryPosition::end(bool) { chassis->input(Vector2f(0.0f), 0.0f); }
