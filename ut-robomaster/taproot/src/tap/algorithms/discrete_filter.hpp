ff8c2a04faaa19606c425879afd238aee5a2c14a
suhas <hi@suhas.one>
2025-06-17 01:52:35 -0500

    regenerate taproot

--- ut-robomaster/taproot/src/tap/algorithms/discrete_filter.hpp
+++ ut-robomaster/taproot/src/tap/algorithms/discrete_filter.hpp
@@ -27,26 +27,10 @@
 #include <array>
 #include <cstdint>
 
-namespace tap::algorithms::filter
+namespace tap
 {
-/**
- * @struct Coefficients
- * @brief Represents the coefficients used in a discrete filter.
- *
- * This structure holds two sets of coefficients:
- * - `naturalResponseCoefficients`: Coefficients related to the natural response of the system.
- * - `forcedResponseCoefficients`: Coefficients related to the forced response of the system.
- *
- * @tparam T The data type of the coefficients (e.g., float, double).
- * @tparam SIZE The size of the coefficient arrays.
- */
-template <uint8_t SIZE, typename T = float>
-struct Coefficients
+namespace algorithms
 {
-    std::array<T, SIZE> naturalResponseCoefficients;
-    std::array<T, SIZE> forcedResponseCoefficients;
-};
-
 /**
  * @brief DiscreteFilter class implements a discrete-time filter using the finite difference
  * equation.
@@ -68,17 +52,10 @@ public:
      * state to zero.
      */
     DiscreteFilter(
-        std::array<T, SIZE> naturalResponseCoefficients,
-        std::array<T, SIZE> forcedResponseCoefficients)
+        std::array<T, SIZE> &naturalResponseCoefficients,
+        std::array<T, SIZE> &forcedResponseCoefficients)
         : naturalResponseCoefficients(naturalResponseCoefficients),
           forcedResponseCoefficients(forcedResponseCoefficients)
-    {
-        reset();
-    }
-
-    DiscreteFilter(const Coefficients<SIZE, T> coefficients)
-        : naturalResponseCoefficients(coefficients.naturalResponseCoefficients),
-          forcedResponseCoefficients(coefficients.forcedResponseCoefficients)
     {
         reset();
     }
@@ -144,20 +121,6 @@ public:
         return 0.0f;
     }
 
-    void setCoefficients(Coefficients<SIZE, T> coe)
-    {
-        this->naturalResponseCoefficients = coe.naturalResponseCoefficients;
-        this->forcedResponseCoefficients = coe.forcedResponseCoefficients;
-    }
-
-    void setCoefficients(
-        std::array<T, SIZE> naturalResponseCoefficients,
-        std::array<T, SIZE> forcedResponseCoefficients)
-    {
-        this->naturalResponseCoefficients = naturalResponseCoefficients;
-        this->forcedResponseCoefficients = forcedResponseCoefficients;
-    }
-
 private:
     std::array<T, SIZE> naturalResponseCoefficients;
     std::array<T, SIZE> forcedResponseCoefficients;
@@ -165,6 +128,8 @@ private:
     std::array<T, SIZE> forcedResponse;
 };
 
-}  // namespace tap::algorithms::filter
+}  // namespace algorithms
+
+}  // namespace tap
 
 #endif  // TAPROOT_DISCRETE_FILTER_HPP_
