8ecec064f3797c73703d81bffa1ac2b344b14726
Suhas <suhas@futo.org>
2025-06-25 04:00:26 +0000

    patch taproot!!!!

--- ut-robomaster/taproot/src/tap/communication/sensors/imu/bmi088/bmi088.hpp
+++ ut-robomaster/taproot/src/tap/communication/sensors/imu/bmi088/bmi088.hpp
@@ -121,7 +121,7 @@ public:
 private:
     Drivers *drivers;
 
-    inline float getAccelerationSensitivity() const override { return ACC_G_PER_ACC_COUNT; }
+    inline float getAccelerationSensitivity() const override { return tap::algorithms::ACCELERATION_GRAVITY / ACC_G_PER_ACC_COUNT; }
 
     static constexpr uint16_t RAW_TEMPERATURE_TO_APPLY_OFFSET = 1023;
     /// Offset parsed temperature reading by this amount if > RAW_TEMPERATURE_TO_APPLY_OFFSET.
