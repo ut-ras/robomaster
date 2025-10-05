17c3d239c2eca20e04f5774ca43d7586cd50b033
George Adamopoulos <georgenkaki@gmail.com>
2025-10-05 20:56:28 +0000

    added flywheel ind + bugfix

--- ut-robomaster/src/control/client-display/command_client_display.cpp
+++ ut-robomaster/src/control/client-display/command_client_display.cpp
@@ -1,72 +1,111 @@
 #include "command_client_display.hpp"
 
+#define PT_AWAIT_RESUMABLE(expr)                                      \
+    do                                                                \
+    {                                                                 \
+        this->ptState = __LINE__;                                     \
+        modm_fallthrough;                                             \
+        case __LINE__:                                                \
+        {                                                             \
+            auto _rf = (expr);                                        \
+            if (_rf.getState() > modm::rf::NestingError) return true; \
+            _rf.getResult(); /* void OK */                            \
+        }                                                             \
+    } while (0)
+/*
 
-modm::ResumableResult<bool> BeybladeIndicator::sendInitialGraphics()
+Caleb's Beyblade indicator
+*/
+modm::ResumableResult<void> BeybladeIndicator::sendInitialGraphics()
 {
-	// The number represents the index of the resumable function in this class
-	RF_BEGIN(0);
-	
-	RF_CALL(refSerialTransmitter.sendGraphic(&msg));
+    // The number represents the index of the resumable function in this class
 
-	RF_END();
+    return refSerialTransmitter.sendGraphic(&msg);
 }
 
-modm::ResumableResult<bool> BeybladeIndicator::update()
+modm::ResumableResult<void> BeybladeIndicator::update()
 {
-	// This is the second resumable function so its index is 1
-	RF_BEGIN(1);
+    // This is the second resumable function so its index is 1
 
-	RF_CALL(refSerialTransmitter.sendGraphic(&msg));
-
-	RF_END();
+    return refSerialTransmitter.sendGraphic(&msg);
 }
 
 void BeybladeIndicator::initialize()
 {
-		RefSerialTransmitter::configGraphicGenerics(
-			&msg.graphicData[0],
-			graphicName,
-			RefSerialData::Tx::GRAPHIC_ADD,
-			1, // Graphic layer can be 0-9
-			RefSerialData::Tx::GraphicColor::PINK
-			);
-		RefSerialTransmitter::configCircle(
-			50,
-			1920/2,
-		    1080/2,
-            200,
-			&msg.graphicData[0]
-			);
-	}
+    RefSerialTransmitter::configGraphicGenerics(
+        &msg.graphicData[0],
+        graphicName,
+        RefSerialData::Tx::GRAPHIC_ADD,
+        1,                                       // Graphic layer can be 0-9
+        RefSerialData::Tx::GraphicColor::PINK);  // is there a way to change this \/\/ to be a tank
+                                                 // kind of thing
+    RefSerialTransmitter::configCircle(50, 1920 / 2, 1080 / 2, 200, &msg.graphicData[0]);
+}
+
+/*
+George's Flywheel indicator
+*/
+
+modm::ResumableResult<void> FlywheelIndicator::sendInitialGraphics()
+{
+    // The number represents the index of the resumable function in this class
+
+    return refSerialTransmitter.sendGraphic(&msg);
+}
+
+modm::ResumableResult<void> FlywheelIndicator::update()
+{
+    // This is the second resumable function so its index is 1
+
+    return refSerialTransmitter.sendGraphic(&msg);
+}
+
+void FlywheelIndicator::initialize()
+{
+    RefSerialTransmitter::configGraphicGenerics(
+        &msg.graphicData[0],
+        graphicName,
+        RefSerialData::Tx::GRAPHIC_ADD,
+        1,  // Graphic layer can be 0-9
+        RefSerialData::Tx::GraphicColor::GREEN);
+
+    // ADJUST APPEARANCE \/ \/
+    RefSerialTransmitter::configCircle(50, 1920 / 2, 1080 / 2, 200, &msg.graphicData[0]);
+}
 
 namespace commands
 {
 
 void CommandClientDisplay::restartHud()
 {
-	beybladeIndicator.initialize();
-
-	this->restarting = false;
+    beybladeIndicator.initialize();
+    flywheelIndicator.initialize();
+    this->restarting = false;
 }
 
 bool CommandClientDisplay::run()
 {
-	if (!this->isRunning()) {
-		restart();
-		this->restartHud();
-	}
+    if (!this->isRunning())
+    {
+        restart();
+        this->restartHud();
+    }
 
-	PT_BEGIN();
-	
-	PT_WAIT_UNTIL(drivers->refSerial.getRefSerialReceivingData());
+    PT_BEGIN();
 
-	PT_CALL(beybladeIndicator.sendInitialGraphics());
+    PT_WAIT_UNTIL(drivers->refSerial.getRefSerialReceivingData());
 
-	while (!this->restarting) {
-		PT_CALL(beybladeIndicator.update());
-		PT_YIELD();
-	}
+    // Initial draws
+    PT_AWAIT_RESUMABLE(beybladeIndicator.sendInitialGraphics());
+    PT_AWAIT_RESUMABLE(flywheelIndicator.sendInitialGraphics());
 
-	PT_END();
+    while (!this->restarting)
+    {
+        PT_AWAIT_RESUMABLE(beybladeIndicator.update());
+        PT_AWAIT_RESUMABLE(flywheelIndicator.update());
+        PT_YIELD();
+    }
+
+    PT_END();
 }
-}
+}  // namespace commands
--- ut-robomaster/src/control/client-display/command_client_display.hpp
+++ ut-robomaster/src/control/client-display/command_client_display.hpp
@@ -1,41 +1,58 @@
 #pragma once
 
-#include "tap/control/command.hpp"
-#include "tap/communication/serial/ref_serial_transmitter.hpp"
-#include "tap/communication/serial/ref_serial_data.hpp"
-#include "modm/processing/protothread.hpp"
-#include "modm/processing/resumable.hpp"
-
 #include "tap/communication/sensors/buzzer/buzzer.hpp"
-
-#include "drivers.hpp"
+#include "tap/communication/serial/ref_serial_data.hpp"
+#include "tap/communication/serial/ref_serial_transmitter.hpp"
+#include "tap/control/command.hpp"
 
 #include "control/client-display/client_display_subsystem.hpp"
-
+#include "modm/processing/protothread.hpp"
+#include "modm/processing/resumable.hpp"
 #include "subsystems/chassis/chassis_subsystem.hpp"
+
+#include "drivers.hpp"
 
 using namespace tap::control;
 using namespace tap::communication::serial;
 
-using subsystems::control::ClientDisplaySubsystem;
 using subsystems::chassis::ChassisSubsystem;
+using subsystems::control::ClientDisplaySubsystem;
 
 class BeybladeIndicator : protected modm::Resumable<2>
 {
 private:
-	const uint8_t* graphicName = (uint8_t*)"\x00\x00\x01";
-	RefSerialTransmitter &refSerialTransmitter;
-	RefSerialData::Tx::Graphic5Message msg;
+    const uint8_t *graphicName = (const uint8_t *)"\x00\x00\x01";
+    RefSerialTransmitter &refSerialTransmitter;
+    RefSerialData::Tx::Graphic5Message msg;
 
 public:
-	BeybladeIndicator(RefSerialTransmitter refSerialTransmitter)
-	: refSerialTransmitter(refSerialTransmitter)
-	{}
+    explicit BeybladeIndicator(RefSerialTransmitter &refSerialTransmitter)
+        : refSerialTransmitter(refSerialTransmitter)
+    {
+    }
 
-	modm::ResumableResult<bool> sendInitialGraphics();
-	modm::ResumableResult<bool> update();
-	
-	void initialize();
+    modm::ResumableResult<void> sendInitialGraphics();
+    modm::ResumableResult<void> update();
+
+    void initialize();
+};
+
+class FlywheelIndicator : protected modm::Resumable<2>
+{
+private:
+    const uint8_t *graphicName = (const uint8_t *)"\x00\x00\x02";
+    RefSerialTransmitter &refSerialTransmitter;
+    RefSerialData::Tx::Graphic5Message msg;
+
+public:
+    explicit FlywheelIndicator(RefSerialTransmitter &refSerialTransmitter)
+        : refSerialTransmitter(refSerialTransmitter)
+    {
+    }
+
+    void initialize();
+    modm::ResumableResult<void> sendInitialGraphics();
+    modm::ResumableResult<void> update();
 };
 
 namespace commands
@@ -43,38 +60,36 @@ namespace commands
 class CommandClientDisplay : public Command, modm::pt::Protothread
 {
 private:
-	src::Drivers* drivers;
-	RefSerialTransmitter refSerialTransmitter;
-	BeybladeIndicator beybladeIndicator;
-	bool restarting = true;
+    src::Drivers *drivers;
+    RefSerialTransmitter refSerialTransmitter;
+    BeybladeIndicator beybladeIndicator;
+    FlywheelIndicator flywheelIndicator;
+    bool restarting = true;
 
-	void restartHud();
+    void restartHud();
+
 public:
-	CommandClientDisplay(
-		src::Drivers *drivers,
-		ChassisSubsystem *chassis)
-			: Command(),
-			drivers(drivers),
-			refSerialTransmitter(drivers),
-			beybladeIndicator(refSerialTransmitter)
-	{
-		addSubsystemRequirement(chassis);
-	}
-	
-	bool run();
-	
-	const char *getName() const override { return "client display"; }
-	void initialize() override
-	{
-		tap::buzzer::playNote(&drivers->pwm, 880);
-		this->restarting = true;
-	};
+    CommandClientDisplay(src::Drivers *drivers, ChassisSubsystem *chassis)
+        : Command(),
+          drivers(drivers),
+          refSerialTransmitter(drivers),
+          beybladeIndicator(refSerialTransmitter),
+          flywheelIndicator(refSerialTransmitter)
+    {
+        addSubsystemRequirement(chassis);
+    }
 
-	void execute() override
-	{
-		run();
-	}
-	void end(bool) override {}
-	bool isFinished() const override { return false; }
+    bool run();
+
+    const char *getName() const override { return "client display"; }
+    void initialize() override
+    {
+        tap::buzzer::playNote(&drivers->pwm, 880);
+        this->restarting = true;
+    };
+
+    void execute() override { run(); }
+    void end(bool) override {}
+    bool isFinished() const override { return false; }
 };
-}
+}  // namespace commands
